9e1adfee7a9fc66bdce11b86dd47c1345f2b34bf
Allen's PC <ale21100@gmail.com>
2021-11-15 04:12:25 -0800

    Added Pojo for employee

---
+++ src/main/java/Pojos/Employee.java
@@ -0,0 +1,57 @@
+package Pojos;
+
+public class Employee {
+    private String name;
+    private String gender;
+    private int age;
+    private int id;
+
+    public Employee(String name, String gender, int age, int id) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
+}
