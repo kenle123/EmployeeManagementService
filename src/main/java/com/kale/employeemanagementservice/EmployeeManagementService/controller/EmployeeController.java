a93aa346dcae871f79375dcceaed49ff087a2122
Allen's PC <ale21100@gmail.com>
2021-11-18 01:35:44 -0800

    Created service layer and dependency injection for service and controller

--- src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/controller/EmployeeController.java
+++ src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.kale.employeemanagementservice.EmployeeManagementService.controller;
 
 import com.kale.employeemanagementservice.EmployeeManagementService.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/employee")
+
 public class EmployeeController {
-    @GetMapping("api/v1/employee")
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
     public List<Employee> getEmployee() {
-        return List.of(new Employee("Bob Jones", "Male", 22, 99999, 25000, "Janitor"));
+        return employeeService.getEmployee();
     }
 }
