24cc669aaef5be40dfccc38dd20a9dc520c277f4
Allen <ale21100@gmail.com>
2021-12-20 23:34:24 -0800

    Modified POST request to insert user's email and password to database

--- src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/EmployeeManagementServiceApplication.java
+++ src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/EmployeeManagementServiceApplication.java
@@ -1,7 +1,10 @@
 package com.kale.employeemanagementservice.EmployeeManagementService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/controller/RegisterController.java
+++ src/main/java/com/kale/employeemanagementservice/EmployeeManagementService/controller/RegisterController.java
@@ -1,16 +1,33 @@
 package com.kale.employeemanagementservice.EmployeeManagementService.controller;
-
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.kale.employeemanagementservice.EmployeeManagementService.model.Admin;
+import com.kale.employeemanagementservice.EmployeeManagementService.repository.AdminRepository;
+import org.hibernate.engine.jdbc.connections.spi.JdbcConnectionAccess;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 @Controller
-public class RegisterController {
+public class RegisterController  {
+
+    @Autowired
+    public JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AdminRepository adminRepository;
+
+
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
         model.addAttribute("admin", new Admin());
@@ -18,11 +35,26 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String postRegisterCredentials(@ModelAttribute Admin admin, Model model) {
+    public String postRegisterCredentials(@ModelAttribute @RequestBody Admin admin, Model model) throws SQLException {
+
         String email = admin.getEmail();
         String password = admin.getPassword();
 
-        System.out.println("email is: " + email + " password is: " + password);
+        String insertString = "INSERT INTO admin" + "  (id, email, password) VALUES " + " (?, ?, ?);";
+
+        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/employee", "postgres", "kekw123");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(insertString)) {
+            preparedStatement.setInt(1,22);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+
+        }
+
+
         return "register";
     }
 }
